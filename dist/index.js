f586b0cc19348108bd0c234fe1a1345fcc31a45a
Hridoy Mnd <hridoymnd10@gmail.com>
2025-05-08 18:09:28 +0600

    problem 7 solution done

--- dist/index.js
+++ dist/index.js
@@ -54,3 +54,21 @@ function getMostExpensiveProduct(products) {
         return mostExpensive;
     }
 }
+var Day;
+(function (Day) {
+    Day[Day["Monday"] = 0] = "Monday";
+    Day[Day["Tuesday"] = 1] = "Tuesday";
+    Day[Day["Wednesday"] = 2] = "Wednesday";
+    Day[Day["Thursday"] = 3] = "Thursday";
+    Day[Day["Friday"] = 4] = "Friday";
+    Day[Day["Saturday"] = 5] = "Saturday";
+    Day[Day["Sunday"] = 6] = "Sunday";
+})(Day || (Day = {}));
+function getDayType(day) {
+    if (Day.Saturday === day || day === Day.Sunday) {
+        return `Today is Weekend`;
+    }
+    else {
+        return `Today is WeekDay`;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -6,64 +6,81 @@ function formatString(input: string, toUpper?: boolean) {
   }
 }
 
-function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
   const HighestRating = items?.filter((item) => item?.rating >= 4);
   return HighestRating;
 }
 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
-    return arrays.reduce((acc, curr) => acc.concat(curr), []);
+  return arrays.reduce((acc, curr) => acc.concat(curr), []);
+}
+
+class Vehicle {
+  private make: string;
+  private year: number;
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
   }
-
-
-  class Vehicle {
-    private make: string;
-    private year: number;
-    constructor(make: string, year: number) {
-      this.make = make;
-      this.year = year;
-    }
-    public getInfo(): string {
-      return `The Car Made by ${this.make} company In ${this.year}`;
-    }
+  public getInfo(): string {
+    return `The Car Made by ${this.make} company In ${this.year}`;
   }
+}
 
-  class Car extends Vehicle {
-    private model: string;
-    constructor(make: string, year: number, model: string) {
-      super(make, year);
-      this.model = model;
-    }
-    public getModel(): string {
-      return `The car model is ${this.model}`;
-    }
+class Car extends Vehicle {
+  private model: string;
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
   }
-
-  function processValue(value: string | number): number {
-    if (typeof value === "string") {
-      return value.length;
-    } else {
-      return value * 2;
-    }
+  public getModel(): string {
+    return `The car model is ${this.model}`;
   }
+}
 
-
-  interface Product {
-    name: string;
-    price: number;
+function processValue(value: string | number): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else {
+    return value * 2;
   }
+}
 
-  function getMostExpensiveProduct(products: Product[]): Product | null {
-    if (products.length === 0) {
-      return null;
-    } else {
-      let mostExpensive = products[0];
-      for (const product of products) {
-        if (product.price > mostExpensive.price) {
-          mostExpensive = product;
-        }
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0) {
+    return null;
+  } else {
+    let mostExpensive = products[0];
+    for (const product of products) {
+      if (product.price > mostExpensive.price) {
+        mostExpensive = product;
       }
-      return mostExpensive;
     }
+    return mostExpensive;
   }
+}
 
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  if (Day.Saturday === day || day === Day.Sunday) {
+    return `Today is Weekend`;
+  } else {
+    return `Today is WeekDay`;
+  }
+}
